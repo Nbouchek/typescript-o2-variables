f6e9e47f6b045e530905b914f1799a77e25aed0b
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-01 11:55:49 +0000

    Variables in typescript use template variables - demo

--- sample-types.ts
+++ sample-types.ts
@@ -1,9 +1,18 @@
-let found: boolean= true;
+let found: boolean = true;
 let grade: number = 88.6;
 let firstName: string = "Nacer";
 let lastName: string = "Bouchekhima";
 
+// lets break it
+// found = 0;
+// garde = "A";
+// firstName = true;
 
 console.log(found);
 console.log("The grade is " + grade);
 console.log("Hi " + firstName + " " + lastName);
+
+
+// use template Strings
+
+console.log(`Hi ${firstName} ${lastName}`);
