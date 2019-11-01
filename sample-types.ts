c05302ab5ce848c010d3a1e4dc53e8fcaa56dd5f
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-01 11:46:12 +0000

    Variables in typescript - demo

---
+++ sample-types.ts
@@ -0,0 +1,9 @@
+let found: boolean= true;
+let grade: number = 88.6;
+let firstName: string = "Nacer";
+let lastName: string = "Bouchekhima";
+
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
